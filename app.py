c156f3ee6e83694712ce4bc796dbece0d9988bf0
Alex240217 <167487543+Alex240217@users.noreply.github.com>
2024-05-26 17:42:08 -0600

    Codigo completo

--- app.py
+++ app.py
@@ -4,11 +4,21 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
-        
-if hist_button: # al hacer clic en el botón
-           
-st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-            
-fig = px.histogram(car_data, x="odometer")
-        
-st.plotly_chart(fig, use_container_width=True)
+
+st.header('Proyecto sprint 5')
+
+# Crear botones
+hist_button = st.button('Construir histograma')
+scatter_button = st.button('Construir gráfico de dispersión')
+
+# Manejar el evento del botón de histograma
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig_hist = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+# Manejar el evento del botón de gráfico de dispersión
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    fig_scatter = px.scatter(car_data, x="odometer", y="price", color="type")
+    st.plotly_chart(fig_scatter, use_container_width=True)
