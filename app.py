5fac0d012aa6ba65e88108a8be95abea186fb7a3
Alex240217 <167487543+Alex240217@users.noreply.github.com>
2024-05-26 18:16:00 -0600

    Cambio en el código

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import pandas as pd
 import plotly.express as px
-import streamlit as st 
+import streamlit as st
 
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
-hist_button = st.button('Construir histograma') # crear un botón
+# Leer los datos
+car_data = pd.read_csv('vehicles_us.csv')
 
+# Título del proyecto
 st.header('Proyecto sprint 5')
+
+# Crear una casilla de verificación
+show_hist = st.checkbox('Mostrar histograma')
 
 # Crear botones
 hist_button = st.button('Construir histograma')
 scatter_button = st.button('Construir gráfico de dispersión')
 
 # Manejar el evento del botón de histograma
-if hist_button and show_hist:
-    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    fig_hist = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig_hist, use_container_width=True)
+if hist_button:
+    if show_hist:
+        st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        fig_hist = px.histogram(car_data, x="odometer")
+        st.plotly_chart(fig_hist, use_container_width=True)
+    else:
+        st.write('Casilla de verificación no seleccionada, no se mostrará el histograma.')
 
 # Manejar el evento del botón de gráfico de dispersión
 if scatter_button:
